45ded8ad41bc4201b282e3f277c58fc84975b934
waddo4 <109324137+waddo4@users.noreply.github.com>
2022-12-20 21:23:17 +1100

    Added stop button for side animations
    Added a button to stop the side animations, need to get it working as a toggle

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const p2score = document.querySelector('.p2score')
 const p1img = document.querySelector('.p1img')
 const p2img = document.querySelector('.p2img')
 const resetBtn = document.querySelector('.reset')
+const stopBtn = document.querySelector('.stop')
+const side1 = document.querySelector('.side1')
+const side2 = document.querySelector('.side2')
+const sideTitle1 = document.querySelector('.sideTitle1')
+const sideTitle2 = document.querySelector('.sideTitle2')
 
     //Players
 const player1 = 'Player1' 
@@ -13,14 +18,14 @@ const player2 = 'Player2'
 const players = [player1, player2]
 let currentPlayer = players[0]
 
-    //Counter for for a draw
+    //Counter for a draw
 let draw = 0
-console.log(tiles.length);
 
     //Changing player's score into a number
 let counter1 = parseInt(p1score.textContent)
 let counter2 = parseInt(p2score.textContent)
 
+    //Board reset button
 resetBtn.addEventListener('click', () => {
     tiles.forEach((tile) => {
         tile.classList.remove(player1, player2)
@@ -30,7 +35,17 @@ resetBtn.addEventListener('click', () => {
     draw = 0
 })
 
+    //Stops sidebar animations
+stopBtn.addEventListener('click', () => {
 
+        side1.style.backgroundImage = 'none';
+        side1.style.opacity = '1';
+        side2.style.backgroundImage = 'none';
+        side2.style.opacity = '1';
+        sideTitle1.style.opacity = '1';
+        sideTitle2.style.opacity = '1';
+        side1.setAttribute('selected', true)
+})
 
     //Function for player turn + changing the currentplayer
         //Attribute portion of the function sourced from chatGPT 
