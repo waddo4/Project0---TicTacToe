8ab59cdcf3ea1add79c87958a251cff013434b37
waddo4 <109324137+waddo4@users.noreply.github.com>
2022-12-20 15:38:52 +1100

    Draw included, scores now update
    Draw if nobody wins within the max amount of moves, scores updates if a player wins.

--- README.md
+++ README.md
@@ -12,21 +12,22 @@ My game will include out of this world features.
 
 ## Planning & Development
 
-Planning is underway. Development has begun.
+Planning is underway. Development is coming along swimmingly.
 
 ## Bugs & Solutions
 
-- ~~Potential bug: How to stop players from choosing the same square?~~  SOLVED
+- ~~Potential bug: How to stop players from choosing the same square?~~  SOLVED (not really)
 - ~~D.R.Y bug: Repeating functions for each grid square.~~ SOLVED
-- .removeEventListener not working now functions are not hard coded.
+- .removeEventListener not working now functions are not hard coded. SOLVED 
+    - Adds an attribute to the tile when it's clicked. Cnce that attribute is added, the eventListener ignores that tile.
 
 ## Improvement ideas
 
-Get the game working.
+Make the game good.
 
 ## Acknowledgements
 
-
+Thank you Dido & CJ for all your help and support throughout the project.
 
 ---
 
--- script.js
+++ script.js
@@ -6,33 +6,43 @@ const p2score = document.querySelector('.p2score')
 const p1img = document.querySelector('.p1img')
 const p2img = document.querySelector('.p2img')
 
-    //players
+    //Players
 const player1 = 'Player1' 
 const player2 = 'Player2'
 const players = [player1, player2]
 let currentPlayer = players[0]
 
-//might be helpful
-const playerArray = []
+    //Counter for for a draw
+let draw = 0
+console.log(tiles.length);
 
-    //loop for the # of squares
-// for (let i=0; i<grid.children.length; i++)
+    //Counter for player score
+let counter1 = parseInt(p1score.textContent)
+let counter2 = parseInt(p2score.textContent)
 
     //Function for player turn + changing the currentplayer
         //Attribute portion of the function sourced from chatGPT 
 tiles.forEach((tile) => {
     tile.addEventListener('click', () => {
      
+            //Returns if the tile selected has already been clicked.
         if (tile.getAttribute('selected') === 'true') {
             return;
         }
-  
+
+            //What happens to the tile after it's clicked.
         tile.setAttribute('selected', 'true');
         tile.setAttribute('player', currentPlayer);
             tile.classList.add(currentPlayer)
             players.reverse()
             currentPlayer = players[0]
             playerTurn.textContent = `${currentPlayer}'s turn`
+            draw++
+
+            //Draw condition
+            if (draw === tiles.length) {
+                playerTurn.textContent = `It's a draw!`
+            }
 
             //Player 1 win conditions
         if (tiles[0].getAttribute('player') === 'Player1' && tiles[1].getAttribute('player') === 'Player1' && tiles[2].getAttribute('player') === 'Player1' || 
@@ -44,6 +54,8 @@ tiles.forEach((tile) => {
         tiles[0].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[8].getAttribute('player') === 'Player1'|| 
         tiles[2].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[6].getAttribute('player') === 'Player1') {
             playerTurn.textContent = `${players[1]} is the winner!`
+            counter1 += 1000
+            p1score.innerHTML = counter1
             tiles.forEach((tile) => {
                 tile.setAttribute('selected', 'true')
             })
@@ -59,6 +71,8 @@ tiles.forEach((tile) => {
         tiles[0].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[8].getAttribute('player') === 'Player2'|| 
         tiles[2].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[6].getAttribute('player') === 'Player2') {
             playerTurn.textContent = `${players[1]} is the winner!`
+            counter2 += 1000
+            p2score.innerHTML = counter2
             tiles.forEach((tile) => {
                 tile.setAttribute('selected', 'true')
             })
