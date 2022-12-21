19fc56a7fc374f76942bad316a925b3d85695513
waddo4 <109324137+waddo4@users.noreply.github.com>
2022-12-21 15:37:49 +1100

    Side animation toggle working + style toggle added
    Side animation button now toggles instead of just turning off. Added a style swap toggle to go dark mode and light mode. Still need to change player images

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    // Elements getting selected
+// Elements getting selected
 const tiles = document.querySelectorAll('.tile')
 const playerTurn = document.querySelector('.playerTurn')
 const p1score = document.querySelector('.p1score')
@@ -9,23 +9,27 @@ const resetBtn = document.querySelector('.reset')
 const stopBtn = document.querySelector('.stop')
 const side1 = document.querySelector('.side1')
 const side2 = document.querySelector('.side2')
-const sideTitle1 = document.querySelector('.sideTitle1')
-const sideTitle2 = document.querySelector('.sideTitle2')
+const sideTitle1 = document.querySelector('.sideT1')
+const sideTitle2 = document.querySelector('.sideT2')
+const style = document.querySelector('.style')
+const container = document.querySelector('.container')
+const player1Title = document.querySelector('.player1Title')
+const player2Title = document.querySelector('.player2Title')
 
-    //Players
-const player1 = 'Player1' 
+//Players
+const player1 = 'Player1'
 const player2 = 'Player2'
 const players = [player1, player2]
 let currentPlayer = players[0]
 
-    //Counter for a draw
+//Counter for a draw
 let draw = 0
 
-    //Changing player's score into a number
+//Changing player's score into a number
 let counter1 = parseInt(p1score.textContent)
 let counter2 = parseInt(p2score.textContent)
 
-    //Board reset button
+//Board reset button
 resetBtn.addEventListener('click', () => {
     tiles.forEach((tile) => {
         tile.classList.remove(player1, player2)
@@ -35,29 +39,47 @@ resetBtn.addEventListener('click', () => {
     draw = 0
 })
 
-    //Stops sidebar animations
+//Stops sidebar animations
 stopBtn.addEventListener('click', () => {
 
-        side1.style.backgroundImage = 'none';
-        side1.style.opacity = '1';
-        side2.style.backgroundImage = 'none';
-        side2.style.opacity = '1';
-        sideTitle1.style.opacity = '1';
-        sideTitle2.style.opacity = '1';
-        side1.setAttribute('selected', true)
+    side1.classList.toggle("side1Off")
+    side2.classList.toggle("side2Off")
+    sideTitle1.classList.toggle("sideT1On")
+    sideTitle2.classList.toggle("sideT2On")
+
 })
 
-    //Function for player turn + changing the currentplayer
-        //Attribute portion of the function sourced from chatGPT 
+//Darkmode toggle
+style.addEventListener('click', () => {
+
+    container.classList.toggle("containerInv")
+    // player1.textContent.toggle("invader2")
+    // player2.textContent.toggle("spaceship1")
+    player1Title.classList.toggle("playerTitleInv")
+    player2Title.classList.toggle("playerTitleInv")
+    stopBtn.classList.toggle("stopInv")
+    resetBtn.classList.toggle("resetInv")
+    sideTitle1.classList.toggle("sideTitleInv")
+    sideTitle2.classList.toggle("sideTitleInv")
+    side1.classList.toggle("sideInv")
+    side2.classList.toggle("sideInv")
+    tiles.forEach((tile) => {
+        tile.classList.toggle("tileInv")
+    });
+    style.classList.toggle("styleInv")
+
+})
+
+//Function for player turn + changing the currentplayer
 tiles.forEach((tile) => {
     tile.addEventListener('click', () => {
-     
-            //Returns if the tile selected has already been clicked.
+
+        //Returns if the tile selected has already been clicked.
         if (tile.getAttribute('selected') === 'true') {
             return;
         }
 
-            //What happens to the tile after it's clicked.
+        //What happens to the tile after it's clicked.
         tile.setAttribute('selected', 'true');
         tile.setAttribute('player', currentPlayer);
         tile.classList.add(currentPlayer)
@@ -66,20 +88,20 @@ tiles.forEach((tile) => {
         playerTurn.textContent = `${currentPlayer}'s turn`
         draw++
 
-            //Draw condition
+        //Draw condition
         if (draw === tiles.length) {
-             playerTurn.textContent = `It's a draw!`
+            playerTurn.textContent = `It's a draw!`
         }
 
-            //Player 1 win conditions
-        if (tiles[0].getAttribute('player') === 'Player1' && tiles[1].getAttribute('player') === 'Player1' && tiles[2].getAttribute('player') === 'Player1' || 
-        tiles[3].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[5].getAttribute('player') === 'Player1' || 
-        tiles[6].getAttribute('player') === 'Player1' && tiles[7].getAttribute('player') === 'Player1' && tiles[8].getAttribute('player') === 'Player1' || 
-        tiles[0].getAttribute('player') === 'Player1' && tiles[3].getAttribute('player') === 'Player1' && tiles[6].getAttribute('player') === 'Player1' || 
-        tiles[1].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[7].getAttribute('player') === 'Player1' || 
-        tiles[2].getAttribute('player') === 'Player1' && tiles[5].getAttribute('player') === 'Player1' && tiles[8].getAttribute('player') === 'Player1' || 
-        tiles[0].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[8].getAttribute('player') === 'Player1'|| 
-        tiles[2].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[6].getAttribute('player') === 'Player1') {
+        //Player 1 win conditions
+        if (tiles[0].getAttribute('player') === 'Player1' && tiles[1].getAttribute('player') === 'Player1' && tiles[2].getAttribute('player') === 'Player1' ||
+            tiles[3].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[5].getAttribute('player') === 'Player1' ||
+            tiles[6].getAttribute('player') === 'Player1' && tiles[7].getAttribute('player') === 'Player1' && tiles[8].getAttribute('player') === 'Player1' ||
+            tiles[0].getAttribute('player') === 'Player1' && tiles[3].getAttribute('player') === 'Player1' && tiles[6].getAttribute('player') === 'Player1' ||
+            tiles[1].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[7].getAttribute('player') === 'Player1' ||
+            tiles[2].getAttribute('player') === 'Player1' && tiles[5].getAttribute('player') === 'Player1' && tiles[8].getAttribute('player') === 'Player1' ||
+            tiles[0].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[8].getAttribute('player') === 'Player1' ||
+            tiles[2].getAttribute('player') === 'Player1' && tiles[4].getAttribute('player') === 'Player1' && tiles[6].getAttribute('player') === 'Player1') {
             playerTurn.textContent = `${players[1]} is the winner!`
             counter1 += 1000
             p1score.innerHTML = counter1
@@ -91,15 +113,15 @@ tiles.forEach((tile) => {
             draw = 0
         }
 
-            //Player 2 win conditions (will condense)
-        if (tiles[0].getAttribute('player') === 'Player2' && tiles[1].getAttribute('player') === 'Player2' && tiles[2].getAttribute('player') === 'Player2' || 
-        tiles[3].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[5].getAttribute('player') === 'Player2' || 
-        tiles[6].getAttribute('player') === 'Player2' && tiles[7].getAttribute('player') === 'Player2' && tiles[8].getAttribute('player') === 'Player2' || 
-        tiles[0].getAttribute('player') === 'Player2' && tiles[3].getAttribute('player') === 'Player2' && tiles[6].getAttribute('player') === 'Player2' || 
-        tiles[1].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[7].getAttribute('player') === 'Player2' || 
-        tiles[2].getAttribute('player') === 'Player2' && tiles[5].getAttribute('player') === 'Player2' && tiles[8].getAttribute('player') === 'Player2' || 
-        tiles[0].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[8].getAttribute('player') === 'Player2'|| 
-        tiles[2].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[6].getAttribute('player') === 'Player2') {
+        //Player 2 win conditions (will condense)
+        if (tiles[0].getAttribute('player') === 'Player2' && tiles[1].getAttribute('player') === 'Player2' && tiles[2].getAttribute('player') === 'Player2' ||
+            tiles[3].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[5].getAttribute('player') === 'Player2' ||
+            tiles[6].getAttribute('player') === 'Player2' && tiles[7].getAttribute('player') === 'Player2' && tiles[8].getAttribute('player') === 'Player2' ||
+            tiles[0].getAttribute('player') === 'Player2' && tiles[3].getAttribute('player') === 'Player2' && tiles[6].getAttribute('player') === 'Player2' ||
+            tiles[1].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[7].getAttribute('player') === 'Player2' ||
+            tiles[2].getAttribute('player') === 'Player2' && tiles[5].getAttribute('player') === 'Player2' && tiles[8].getAttribute('player') === 'Player2' ||
+            tiles[0].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[8].getAttribute('player') === 'Player2' ||
+            tiles[2].getAttribute('player') === 'Player2' && tiles[4].getAttribute('player') === 'Player2' && tiles[6].getAttribute('player') === 'Player2') {
             playerTurn.textContent = `${players[1]} is the winner!`
             counter2 += 1000
             p2score.innerHTML = counter2
